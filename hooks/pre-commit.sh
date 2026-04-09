487a4efbd66ff4fcf34ad59f709f86c3d9abd670
SirCesarium <cesarium3600@gmail.com>
2026-04-09 17:28:56 -0400

    chore: create pre-commit hook

---
+++ hooks/pre-commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Running cargo fmt..."
+cargo fmt -- --check
+
+echo "Running cargo check (including tests and benches)..."
+cargo check --all-targets --all-features
+
+echo "Running clippy..."
+cargo clippy --all-targets --all-features -- -D warnings -W clippy::all -W clippy::nursery -W clippy::pedantic
+
+echo "Running sweet..."
+swt --quiet
+
+echo "All checks passed!"
